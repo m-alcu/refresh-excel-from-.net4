4f31d23bf53a2f82797a96b89f0c4baeb2ab6fe5
m-alcu <martin.alcubierre@gmail.com>
2021-02-05 18:46:15 +0100

    POC refresh excel from .net program

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ConsoleApp1
+{
+
+    using Excel = Microsoft.Office.Interop.Excel;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Excel.Application application = new Excel.Application();
+            Excel.Workbook workbook = application.Workbooks.Open(@"C:\proy\Excel\final2.xlsx");
+            workbook.RefreshAll();
+            System.Threading.Thread.Sleep(5000);
+            application.DisplayAlerts = false;
+            workbook.Save();
+            workbook.Close(false, @"C:\proy\Excel\final2.xlsx", null);
+            application.Quit();
+            workbook = null;
+            System.Runtime.InteropServices.Marshal.ReleaseComObject(application);
+        }
+    }
+}
