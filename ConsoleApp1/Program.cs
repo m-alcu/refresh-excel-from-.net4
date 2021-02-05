89d5bc507b6d2cfad17f102e05c6c017a5a82db4
m-alcu <martin.alcubierre@gmail.com>
2021-02-05 21:19:37 +0100

    Parametre de fitxers

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,11 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using Util;
 
-
-namespace ConsoleApp1
+namespace RefreshDatabase
 {
 
     using Excel = Microsoft.Office.Interop.Excel;
@@ -14,15 +11,64 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+
+            Console.WriteLine("**********************************************");
+            Console.WriteLine("***************  INICI   *********************");
+            Console.WriteLine("******* ACTUALITZACIO DE DOCUMENTS ***********");
+            Console.WriteLine("**********************************************");
+            Console.WriteLine("**********************************************");
+
+            string configPath = Directory.GetCurrentDirectory() + "\\config.ini";
+            PropertiesUtility properties = new PropertiesUtility();
+            properties.LoadProperties(configPath);
+
+            string timeoutStr = properties.GetProperty("timeout");
+            Console.Write("Espera per document: " + timeoutStr + " millisegons");
+            Console.WriteLine();
+            int timeout = Int32.Parse(timeoutStr);
+
             Excel.Application application = new Excel.Application();
-            Excel.Workbook workbook = application.Workbooks.Open(@"C:\proy\Excel\final2.xlsx");
-            workbook.RefreshAll();
-            System.Threading.Thread.Sleep(5000);
-            application.DisplayAlerts = false;
-            workbook.Save();
-            workbook.Close(false, @"C:\proy\Excel\final2.xlsx", null);
+
+            string path = Directory.GetCurrentDirectory()+ "\\Lista.xlsx";
+            Excel.Workbook lista = application.Workbooks.Open(path);
+            Excel.Worksheet mySheet = (Excel.Worksheet)lista.Sheets["Hojas"];
+
+            string[] terms = new string[400];
+            int numTerms = 0;
+            Excel.Range dataRange = null;
+            for (int row = 1; row < mySheet.Rows.Count; row++) 
+            {
+                dataRange = (Excel.Range)mySheet.Cells[row, 1];
+                if (dataRange.Value2 == null) break;
+                if (row > 400) break;
+                terms[row-1] = String.Format(dataRange.Value2.ToString());
+                numTerms++;
+            }
+            Console.WriteLine();
+            lista.Close(false, path, null);
+
+
+            for (int i = 0; i < numTerms; i++)
+            {
+                Excel.Workbook workbook = application.Workbooks.Open(terms[i]);
+                workbook.RefreshAll();
+                Console.Write(i+1+". Actualitzant... "+ terms[i]);
+                System.Threading.Thread.Sleep(timeout);
+                Console.Write(" ok");
+                Console.WriteLine();
+                application.DisplayAlerts = false;
+                workbook.Save();
+                workbook.Close(false, terms[i], null);
+                workbook = null;
+            }
+
+            Console.WriteLine("**********************************************");
+            Console.WriteLine("***************    FI    *********************");
+            Console.WriteLine("******* ACTUALITZACIO DE DOCUMENTS ***********");
+            Console.WriteLine("**********************************************");
+            Console.WriteLine("**********************************************");
+
             application.Quit();
-            workbook = null;
             System.Runtime.InteropServices.Marshal.ReleaseComObject(application);
         }
     }
---
+++ ConsoleApp1/Util.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Text.RegularExpressions;
+
+namespace Util
+{
+    public class PropertiesUtility
+    {
+        private static Hashtable ht = new Hashtable();
+        public void LoadProperties(string path)
+        {
+            string[] lines = System.IO.File.ReadAllLines(path);
+            bool readFlag = false;
+            foreach (string line in lines)
+            {
+                string text = Regex.Replace(line, @"\s+", "");
+                readFlag = CheckSyntax(text);
+                if (readFlag)
+                {
+                    string[] splitText = text.Split('=');
+                    ht.Add(splitText[0].ToLower(), splitText[1]);
+                }
+            }
+        }
+
+        private bool CheckSyntax(string line)
+        {
+            if (String.IsNullOrEmpty(line) || line[0].Equals('['))
+            {
+                return false;
+            }
+
+            if (line.Contains("=") && !String.IsNullOrEmpty(line.Split('=')[0]) && !String.IsNullOrEmpty(line.Split('=')[1]))
+            {
+                return true;
+            }
+            else
+            {
+                throw new Exception("Can not Parse Properties file please verify the syntax");
+            }
+        }
+
+        public string GetProperty(string key)
+        {
+            if (ht.Contains(key))
+            {
+                return ht[key].ToString();
+            }
+            else
+            {
+                throw new Exception("Property:" + key + "Does not exist");
+            }
+
+        }
+    }
+}
